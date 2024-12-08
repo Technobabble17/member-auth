81d7105d79ec4a2396bb82ba0f71d8443ab6ed04
Technobabble17 <58636640+Technobabble17@users.noreply.github.com>
2024-12-08 14:45:08 -0500

    feat: add Runway package for Members

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use StatamicRadPack\Runway\Traits\HasRunwayResource;
 
 class Member extends Authenticatable
 {
+    use HasRunwayResource;
+    
     protected $fillable = [
         'name',
         'email',
