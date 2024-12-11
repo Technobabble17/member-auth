ccdbf3976a60b15a3360a0b4194060f81190d117
Technobabble17 <58636640+Technobabble17@users.noreply.github.com>
2024-12-11 12:58:15 -0500

    wip: footer nav links to match header nav

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,3 @@
-
-
 <footer class="bg-white dark:bg-gray-900 p-4">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
@@ -9,21 +7,29 @@
             </a>
             <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                 <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                    <a href="/"
+                        class="block {{ request()->is('/') ? 'link-active' : 'link' }} hover:underline me-4 md:me-6">Home</a>
                 </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-                </li>
-                <li>
-                    <a href="#" class="hover:underline">Contact</a>
-                </li>
+                @if (!Auth::guard('members')->check())
+                    <a href="{{ route('login') }}"
+                        class="{{ request()->routeIs('login') ? 'link-active' : 'link' }} hover:underline me-4 md:me-6">Login</a>
+                    <a href="{{ route('member.register.show') }}"
+                        class="{{ request()->routeIs('member.register.show') ? 'link-active' : 'link' }} hover:underline me-4 md:me-6">Register</a>
+                @endif
+                @if (Auth::guard('members')->check())
+                    <li>
+                        <a href="{{ route('member.dashboard') }}"
+                            class="block {{ request()->routeIs('member.dashboard') ? 'link-active' : 'link' }} hover:underline me-4 md:me-6">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('member.index') }}"
+                            class="block {{ request()->routeIs('member.index') ? 'link-active' : 'link' }} hover:underline me-4 md:me-6">Members</a>
+                    </li>
+                @endif
             </ul>
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a
+                href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
     </div>
 </footer>
-
