0e4b8e0080a869e4c509a8cbdac1c8f6107cf176
Technobabble17 <58636640+Technobabble17@users.noreply.github.com>
2024-12-11 12:44:53 -0500

    removal: exclude gsap animation

--- resources/views/member/login.blade.php
+++ resources/views/member/login.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-    <div class="parent max-w-md mx-auto rounded bg-gray-700 p-8 mt-8 pb-16 overflow-hidden shadow shadow-blue-600">
+    <div class="max-w-md mx-auto rounded bg-gray-700 p-8 mt-8 pb-16 overflow-hidden shadow shadow-blue-600">
         <form action="{{ route('member.login') }}" method="post" class="">
             @csrf
             @include('components.input', [
@@ -22,7 +22,7 @@
             ])
 
             <button type="submit"
-                class="follower absolute text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700">Login</button>
+                class="absolute text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700">Login</button>
         </form>
         @if ($errors->any())
             <div class="mt-24">
--- resources/views/member/register.blade.php
+++ resources/views/member/register.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-    <div class="parent max-w-md mx-auto rounded bg-gray-700 p-8 mt-8 pb-16 overflow-hidden shadow shadow-blue-600">
+    <div class="max-w-md mx-auto rounded bg-gray-700 p-8 mt-8 pb-16 overflow-hidden shadow shadow-blue-600">
         <form action="{{ route('member.register') }}" method="post" class="">
             @csrf
             @include('components.input', [
@@ -35,7 +35,7 @@
             ])
 
             <button type="submit"
-                class="follower absolute text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700">Submit</button>
+                class="absolute text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700">Submit</button>
         </form>
         @if ($errors->any())
             <div class="mt-24">
