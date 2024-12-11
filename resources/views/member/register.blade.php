f1ce71346139db85bb5a0cc8b314bed617fbe912
Technobabble17 <58636640+Technobabble17@users.noreply.github.com>
2024-12-11 13:47:56 -0500

    wip: improved tab accessibility states

--- resources/views/member/register.blade.php
+++ resources/views/member/register.blade.php
@@ -35,7 +35,7 @@
             ])
 
             <button type="submit"
-                class="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700">Submit</button>
+                class="text-white focus:ring-4 focus:ring-white focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700">Submit</button>
         </form>
         @if ($errors->any())
             <div class="mt-24">
