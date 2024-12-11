163b32af81c88cfa22893a6b704d98026d849750
Technobabble17 <58636640+Technobabble17@users.noreply.github.com>
2024-12-11 13:20:42 -0500

    fix: remove layout issues from gsap implementation

--- resources/views/member/login.blade.php
+++ resources/views/member/login.blade.php
@@ -22,7 +22,7 @@
             ])
 
             <button type="submit"
-                class="absolute text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700">Login</button>
+                class="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700">Login</button>
         </form>
         @if ($errors->any())
             <div class="mt-24">
