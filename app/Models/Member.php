919ad6454102627d42e18e3d85fdec1921d63a42
Technobabble17 <58636640+Technobabble17@users.noreply.github.com>
2024-12-12 16:52:44 -0500

    wip: member model adds hasmany transaction relation

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -2,16 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use StatamicRadPack\Runway\Traits\HasRunwayResource;
+use App\Models\Transaction; 
+
 
 class Member extends Authenticatable
 {
     use HasRunwayResource;
-    
+
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
