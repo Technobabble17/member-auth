d54ab262dfe5b3dd824a4eb465e286a25a1d492b
Technobabble17 <58636640+Technobabble17@users.noreply.github.com>
2024-12-15 22:09:40 -0500

    fix: remove dark theming from other templates

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-white dark:bg-gray-900 p-4">
+<footer class="bg-gray-900 p-4">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
             <div class="group overflow-clip relative h-12 md:w-1/3">
@@ -72,7 +72,7 @@
                         .dev</p>
                 </a>
             </div>
-            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium sm:mb-0 text-gray-400">
                 <li>
                     <a href="/"
                         class="block {{ request()->is('/') ? 'link-active' : 'link' }} hover:underline me-4 md:me-6">Home</a>
@@ -95,8 +95,8 @@
                 @endif
             </ul>
         </div>
-        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a
+        <hr class="my-6 sm:mx-auto border-gray-700 lg:my-8" />
+        <span class="block text-sm sm:text-center text-gray-400">© 2023 <a
                 href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
     </div>
 </footer>
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,8 +1,7 @@
-<nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+<nav class="bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-600">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <div class="group overflow-clip relative h-12 md:w-1/3">
-            <a href="{{ route('home') }}" class="flex group/button items-center space-x-3 rtl:space-x-reverse">
-
+            <a href="{{ route('home') }}" class="flex group/button items-center space-x-3">
                 <div class="h-full w-12 absolute bottom-0">
                     <svg class="text-blue-500 group-focus/button:text-white group-hover/button:text-white transition duration-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 258.5 258.5">
                         <path fill="currentColor"
@@ -11,7 +10,7 @@
                 </div>
             </a>
         </div>
-        <div class="flex items-center md:order-2 md:w-1/3 space-x-3 md:space-x-0 md:justify-end rtl:space-x-reverse gap-4">
+        <div class="flex items-center md:order-2 md:w-1/3 space-x-3 md:space-x-0 md:justify-end gap-4">
             @if (Auth::guard('members')->check())
                 <form method="POST" action="{{ route('member.logout') }}">
                     @csrf
@@ -23,7 +22,7 @@
             @endif
             <!-- Mobile menu button -->
             <button data-collapse-toggle="navbar-sticky" type="button"
-                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
                 aria-controls="navbar-sticky" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
                 <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -33,17 +32,11 @@
                 </svg>
             </button>
         </div>
-
         <!-- Collapsible navigation menu -->
-        <div class="md:w-1/3 items-center justify-between hidden max-md:w-full md:flex md:justify-center md:order-1"
-            id="navbar-sticky">
-            <ul
-                class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg
-                       bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white
-                       dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <div id="navbar-sticky" class="md:w-1/3 items-center justify-between hidden max-md:w-full md:flex md:justify-center md:order-1">
+            <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg md:space-x-8 md:flex-row md:mt-0 md:border-0 bg-gray-800 md:bg-gray-900 border-gray-700">
                 <li>
-                    <a href="/"
-                        class="block {{ request()->is('/') ? 'link-active' : 'link' }}">Home</a>
+                    <a href="/" class="block {{ request()->is('/') ? 'link-active' : 'link' }}">Home</a>
                 </li>
                 @if (Auth::guard('members')->check())
                     @include('components.nav-link', [
